4f6d6ce2cd18d723dfd40e1ccf3002848822bce6
Maksym Kovalenko <m.kovalenko.coder@gmail.com>
2023-12-08 02:19:38 +0200

    Second practice part 2 (033)

--- js/script.js
+++ js/script.js
@@ -1,37 +1,73 @@
 'use strict';
 
-const movieDB = {
-        movies: [
-            "Logan",
-            "Justice League",
-            "La La Land",
-            "Whiplash",
-            "Scott Pilgrim vs..."
-        ]
-      },
-      promoAdv = document.querySelectorAll('.promo__adv img'),
-      promoGenre = document.querySelector('.promo__genre'),
-      promoBg = document.querySelector('.promo__bg'),
-      promoList = document.querySelector('.promo__interactive-list');
+document.addEventListener('DOMContentLoaded', () => {
 
-promoAdv.forEach(i => i.remove()); // Deleted ads block
+    const   promoList = document.querySelector('.promo__interactive-list'),
+            formAdd = document.querySelector('form.add'),
+            formInput = formAdd.querySelector('.adding__input'),
+            movieDB = {
+                movies: [
+                    "Logan",
+                    "Justice League",
+                    "La La Land",
+                    "Whiplash",
+                    "Scott Pilgrim vs..."
+                ]
+            };
 
-promoGenre.textContent = "Drama"; // Change genre 'Comedy' to 'Drama'
+    const changeSomeElements = () => {
+        document.querySelectorAll('.promo__adv img').forEach(i => i.remove()); // Deleted ads block
+        document.querySelector('.promo__genre').textContent = "Drama"; // Change genre 'Comedy' to 'Drama'
+        document.querySelector('.promo__bg').style.backgroundImage = 'url("../img/bg.jpg")'; // Change background image
+    };
 
-promoBg.style.backgroundImage = 'url("../img/bg.jpg")'; // Change background image
+    // The list of films on the page is formed based on the movieDB object
+    // Film numbering introduced
 
-movieDB.movies.sort(); // Sort movie list alphabetically
+    formAdd.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-// The list of films on the page is formed based on the movieDB object
-// Film numbering introduced
-promoList.innerHTML = ''; 
+        let newFilm = formInput.value.toUpperCase();
+        const favorite = formAdd.querySelector('[type="checkbox"]').checked;
+        
+        if (newFilm) {
+            if (newFilm.length > 21) newFilm = `${newFilm.substring(0,21)}...`;
+            if (favorite) console.log('Добавляем любимый фильм');
+            movieDB.movies.push(newFilm);
+        }
 
-movieDB.movies.forEach((item, i) => {
-    promoList.innerHTML += `
-        <li class="promo__interactive-item">
-            ${movieDB.movies[i]}
-            <div class="delete"></div>
-        </li>
-    `;
+        e.target.reset();
+        
+        updateList(promoList, movieDB.movies);
+
+    });
+
+
+    function updateList(list, movies) { 
+
+        list.innerHTML = ''; 
+        movies.sort();
+
+        movies.forEach((item, i) => {
+            list.innerHTML += `
+                <li class="promo__interactive-item">
+                    ${i+1}. ${item}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });
+
+        document.querySelectorAll('.delete').forEach((item, i) => {
+            item.addEventListener('click', () => {
+                item.parentElement.remove();
+                movies.splice(i, 1);
+                updateList(promoList, movieDB.movies);
+            })
+        });
+
+    };
+
+    changeSomeElements();
+    updateList(promoList, movieDB.movies);
+
 });
-    
